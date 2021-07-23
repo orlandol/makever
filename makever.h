7bb94cb3d318044bc0d4527507aa85a9eaa29f1a
orlandol <orlandol@veribox.net>
2021-07-23 02:28:56 -0400

    Added alternate versioning scheme.
    makever.h is now divided in two sections:
    1) Major:8.Minor:8.Patch:16 format macros
    2) Major:8.Minor:8.Patch:12.ReleaseType:4 format macros, suffixed with EXT, as in EXTended.

--- makever.h
+++ makever.h
@@ -1,22 +1,37 @@
 #ifndef H_MAKEVER
 #define H_MAKEVER
 
-#define ALPHA_RELEASE 0
-#define RSVD1_RELEASE 1
-#define BETA_RELEASE 2
-#define RELEASE_CANDIDATE 3
-#define STABLE_RELEASE 4
-#define HOTFIX_RELEASE 5
-#define LTS_RELEASE 6
-#define FINAL_RELEASE 7
-
-#define MAKEVER(major, minor, patch, release)\
+/*
+ *  Version format Maj.Min.Patch declarations
+ */
+#define MAKEVER(major, minor, patch)\
   ((((major) & 0xFF) << 24) | (((minor) & 0xFF) << 16) |\
-   (((patch) & 0x1FFF) << 3) | ((release) & 0x07))
+   ((patch) & 0xFFFF))
 
 #define MAJORVER(version) (((version) >> 24) & 0xFF)
 #define MINORVER(version) (((version) >> 16) & 0xFF)
-#define PATCHVER(version) (((version) >> 3) & 0x1FFF)
-#define RELEASETYPE(version) ((version) & 0x7)
+#define PATCHVER(version) ((version) & 0xFFFF)
+
+/*
+ *  Version format Maj.Min.Patch.ReleaseType declarations
+ */
+#define EXPERIMENTAL_RELEASE 7
+#define ALPHA_RELEASE 8
+#define GAMMA_RELEASE 9
+#define BETA_RELEASE 10
+#define RELEASE_CANDIDATE 11
+#define STABLE_RELEASE 12
+#define HOTFIX_RELEASE 13
+#define LTS_RELEASE 14
+#define FINAL_RELEASE 15
+
+#define MAKEVEREXT(major, minor, patch, release)\
+  ((((major) & 0xFF) << 24) | (((minor) & 0xFF) << 16) |\
+   (((patch) & 0x0FFF) << 4) | ((release) & 0x000F))
+
+#define MAJORVEREXT(version) (((version) >> 24) & 0xFF)
+#define MINORVEREXT(version) (((version) >> 16) & 0xFF)
+#define PATCHVEREXT(version) (((version) >> 4) & 0x0FFF)
+#define RELEASETYPEEXT(version) ((version) & 0x000F)
 
 #endif
